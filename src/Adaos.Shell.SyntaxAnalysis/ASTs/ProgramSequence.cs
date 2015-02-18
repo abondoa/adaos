13212b34a324d0bc4d403d94d797193ab0b266c6
Nikolaj Dam Larsen <nikolaj@impero.dk>
2015-02-18 16:41:58 +0100

    Add comments for programsequence AST nodes.

--- src/Adaos.Shell.SyntaxAnalysis/ASTs/ProgramSequence.cs
+++ src/Adaos.Shell.SyntaxAnalysis/ASTs/ProgramSequence.cs
@@ -1,24 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using Adaos.Shell.Interface;
 using Adaos.Shell.Interface.Exceptions;
 
 namespace Adaos.Shell.SyntaxAnalysis.ASTs
 {
-    abstract public class ProgramSequence : AST, IProgramSequence
+    /// <summary>
+    /// An abstract class representing a <see cref="ProgramSequence"/> of the Adaos AST.
+    /// The ProgramSequence is the root node of the AST, and its children are a sequence of commands.
+    /// </summary>
+    public abstract class ProgramSequence : AST, IProgramSequence
     {
+        /// <summary>
+        /// Enumerate the commands of the ProgramSequence.
+        /// </summary>
         public abstract IEnumerable<Command> Commands { get; }
 
-        public ProgramSequence(int position) : base(position) { }
-        public ProgramSequence() { }
+        /// <summary>
+        /// The default constructor for the ProgramSequence.
+        /// </summary>
+        protected ProgramSequence() { }
 
+        /// <summary>
+        /// A constructor for the ProgramSequence.
+        /// </summary>
+        /// <param name="position">The position of the first character in the source code.</param>
+        protected ProgramSequence(int position) : base(position) { }
+
+        /// <summary>
+        /// Enumerate the <see cref="ICommand"/> nodes of the programsequence nodes.
+        /// </summary>
         IEnumerable<ICommand> IProgramSequence<ICommand, AdaosException>.Commands
         {
             get { return Commands; }
         }
 
+        /// <summary>
+        /// Enumerate the errors while executing a program sequence.
+        /// </summary>
         public IEnumerable<AdaosException> Errors
         {
             get;
--- src/Adaos.Shell.SyntaxAnalysis/ASTs/ProgramSequenceActual.cs
+++ src/Adaos.Shell.SyntaxAnalysis/ASTs/ProgramSequenceActual.cs
@@ -1,14 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace Adaos.Shell.SyntaxAnalysis.ASTs
 {
+    /// <summary>
+    /// A class for the actual <see cref="ProgramSequence"/> of the Adaos AST.
+    /// The ProgramSequence is the root node of the AST, and its children are a sequence of commands.
+    /// </summary>
     public class ProgramSequenceActual : ProgramSequence
     {
+        /// <summary>
+        /// Get the first command of the program sequence.
+        /// </summary>
         public Command Command { get; private set; }
+
+        /// <summary>
+        /// Get a <see cref="ProgramSequenceFollow"/> AST node representing the following 
+        /// sequence of commands, after the first command.
+        /// </summary>
         public ProgramSequenceFollow FollowingCommands { get; private set; }
+
+        /// <summary>
+        /// Enumerate the commands of the ProgramSequence.
+        /// </summary>
         public override IEnumerable<Command> Commands 
         {
             get 
@@ -20,6 +33,11 @@ namespace Adaos.Shell.SyntaxAnalysis.ASTs
                 }
             }
         }
+
+        /// <summary>
+        /// Get the position of the first character of the first command of this
+        /// <see cref="ProgramSequenceActual"/> node.
+        /// </summary>
         public override int Position
         {
             get
@@ -28,18 +46,36 @@ namespace Adaos.Shell.SyntaxAnalysis.ASTs
             }
         }
 
-        public ProgramSequenceActual(int position, Command command, ProgramSequenceFollow followingCommands) : base(position)
-        {
-            Command = command;
-            FollowingCommands = followingCommands;
-        }
-
+        /// <summary>
+        /// A constructor for the ProgramSequenceActual
+        /// </summary>
+        /// <param name="command">The first command node of the ProgramSequence.</param>
+        /// <param name="followingCommands">A ProgramSequenceFollow node representing a sequence of commands following the first.</param>
         public ProgramSequenceActual(Command command, ProgramSequenceFollow followingCommands)
         {
             Command = command;
             FollowingCommands = followingCommands;
         }
 
+        /// <summary>
+        /// A constructor for the ProgramSequenceActual
+        /// </summary>
+        /// <param name="position">The position of the first character of this program sequence.</param>
+        /// <param name="command">The first command node of the ProgramSequence.</param>
+        /// <param name="followingCommands">A ProgramSequenceFollow node representing a sequence of commands following the first.</param>
+        public ProgramSequenceActual(int position, Command command, ProgramSequenceFollow followingCommands) : base(position)
+        {
+            Command = command;
+            FollowingCommands = followingCommands;
+        }
+
+
+        /// <summary>
+        /// The visit method used to accept an <see cref="IVisitor"/>, 
+        /// for an implementation of the visitor pattern.
+        /// </summary>
+        /// <param name="visitor">The <see cref="IVisitor"/> to "accept".</param>
+        /// <param name="obj">An object passed to the visitor.</param>
         public override object Visit(IVisitor visitor, object obj)
         {
             return visitor.Visit(this, obj);
